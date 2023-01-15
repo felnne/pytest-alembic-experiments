3a0531dd88e7ff8b078191c1ad8a6f2abf82b624
Felix Fennell <felnne@bas.ac.uk>
2023-01-15 15:41:57 +0000

    resetting migrations between tests
    This doesn’t reset the entire database between tests, but does rollback all migrations first, which I think is good enough for now.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,15 @@
 import pytest
 import sqlalchemy
 
+
 @pytest.fixture
 def alembic_engine():
     """Override this fixture to provide pytest-alembic powered tests with a database handle.
     """
     return sqlalchemy.create_engine("postgresql://felnne@localhost/pytest_alembic")
+
+
+@pytest.fixture
+def ephemeral_alembic_runner(alembic_runner):
+    yield alembic_runner
+    alembic_runner.migrate_down_to('base')
--- tests/test_migrations.py
+++ tests/test_migrations.py
@@ -1,5 +1,19 @@
-from pytest_alembic.tests import test_single_head_revision
-from pytest_alembic.tests import test_upgrade
-from pytest_alembic.tests import test_up_down_consistency
+# noinspection PyUnresolvedReferences
+import pytest
+
+from pytest_alembic.tests import test_single_head_revision as single_head_revision
+from pytest_alembic.tests import test_upgrade as upgrade
+from pytest_alembic.tests import test_up_down_consistency as up_down_consistency
 
 # from pytest_alembic.tests import test_model_definitions_match_ddl
+
+
+class TestMigrations:
+    def test_single_head_revision(self, ephemeral_alembic_runner):
+        single_head_revision(ephemeral_alembic_runner)
+
+    def test_upgrade(self, ephemeral_alembic_runner):
+        upgrade(ephemeral_alembic_runner)
+
+    def test_up_down_consistency(self, ephemeral_alembic_runner):
+        up_down_consistency(ephemeral_alembic_runner)
